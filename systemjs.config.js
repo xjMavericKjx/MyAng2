0217aeb3c227aee47555a19978ac963b51ca84e5
mborodin <mborodin>
2016-11-09 01:47:44 -0800

    Change project files for start

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,6 +21,7 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+            '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
             // other libraries
             'rxjs': 'npm:rxjs',
             'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
